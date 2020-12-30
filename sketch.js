0985f4e41641275ec62ff81e7d031cbd65222cdd
RAJVARDHAN123 <75675172+RAJVARDHAN123@users.noreply.github.com>
2020-12-30 20:39:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 200, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6;
+
+	packageBody = Bodies.circle(width/2 , 100 , 5 , {restitution:0.8, isStatic:false});
+	
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+	World.add(world, packageBody);
+
+	
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+	 World.add(world,packageSprite);
+	
+  
+}
+
+
+function draw() {
+  
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+	keyPressed();
+  
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	// Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+	
+
+	packageSprite.x= packageBody.position.x ;
+	packageSprite.y= packageBody.position.y ;
+    
+  }
+}
+
+
+
